b15d745c6d3d45aff86bd51a633c04e87527d87c
Vincent Shen <edentidus@foxmail.com>
2019-06-11 12:24:35 +0000

    try goka

--- player_api/apphost.go
+++ player_api/apphost.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"github.com/kataras/iris"
+
+	"github.com/vincent-scw/gframe/player_api/kafka"
 )
 
 func main() {
 	app := iris.Default()
 
 	app.Get("health", func(ctx iris.Context) {
-		ctx.Text("I am good.")
+		//ctx.Text("I am good.")
+		p := kafka.NewProducer()
+		p.Test()
 	})
 
 	app.Run(iris.Addr("127.0.0.1:8080"))
