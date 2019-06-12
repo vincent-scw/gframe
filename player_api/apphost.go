03974cd230aa744b7c1fd7d290fe49c21ce4d5cb
Vincent Shen <edentidus@foxmail.com>
2019-06-12 01:10:11 +0000

    producer

--- player_api/apphost.go
+++ player_api/apphost.go
@@ -10,9 +10,12 @@ func main() {
 	app := iris.Default()
 
 	app.Get("health", func(ctx iris.Context) {
-		//ctx.Text("I am good.")
+		ctx.Text("I am good.")
+	})
+
+	app.Post("message", func(ctx iris.Context) {
 		p := kafka.NewProducer()
-		p.Test()
+		p.Emit("my-key", "my-value")
 	})
 
 	app.Run(iris.Addr("127.0.0.1:8080"))
