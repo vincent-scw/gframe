92c920605dabf4deccab66857cfe12ea27a52a48
Vincent Shen <edentidus@foxmail.com>
2019-06-26 08:42:55 +0000

    add register page

---
+++ player_client/src/app/account/register.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class Register extends React.Component {
+	render() {
+		return (
+			<section className="section">
+				<h1 className="title">Register</h1>
+			</section>
+		);
+	}
+}
