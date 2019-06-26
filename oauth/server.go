68be2459433da8db74191af30fe7634c9d095aba
Vincent Shen <edentidus@foxmail.com>
2019-06-26 05:43:17 +0000

    add oauth2 server

--- oauth/server.go
+++ oauth/server.go
@@ -3,8 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/dgrijalva/jwt-go"
+	"github.com/go-session/session"
 	"gopkg.in/oauth2.v3/errors"
+	"gopkg.in/oauth2.v3/generates"
 	"gopkg.in/oauth2.v3/manage"
 	"gopkg.in/oauth2.v3/models"
 	"gopkg.in/oauth2.v3/server"
@@ -13,8 +17,14 @@ import (
 
 func main() {
 	manager := manage.NewDefaultManager()
+	//manager.SetAuthorizeCodeTokenCfg(manage.DefaultAuthorizeCodeTokenCfg)
+	manager.SetImplicitTokenCfg(manage.DefaultImplicitTokenCfg)
+
 	// token memory store
 	manager.MustTokenStorage(store.NewMemoryTokenStore())
+
+	// generate jwt access token
+	manager.MapAccessGenerate(generates.NewJWTAccessGenerate([]byte("00000000"), jwt.SigningMethodHS256))
 
 	// client memory store
 	clientStore := store.NewClientStore()
@@ -29,9 +39,15 @@ func main() {
 	srv.SetAllowGetAccessRequest(true)
 	srv.SetClientInfoHandler(server.ClientFormHandler)
 
+	srv.SetUserAuthorizationHandler(userAuthorizeHandler)
+
 	srv.SetInternalErrorHandler(func(err error) (re *errors.Response) {
 		log.Println("Internal Error:", err.Error())
 		return
+	})
+
+	srv.SetResponseErrorHandler(func(re *errors.Response) {
+		log.Println("Response Error:", re.Error.Error())
 	})
 
 	http.HandleFunc("/authorize", func(w http.ResponseWriter, r *http.Request) {
@@ -45,5 +61,80 @@ func main() {
 		srv.HandleTokenRequest(w, r)
 	})
 
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/auth", authHandler)
+
+	log.Println("Server is running at 9096 port.")
 	log.Fatal(http.ListenAndServe(":9096", nil))
+}
+
+func userAuthorizeHandler(w http.ResponseWriter, r *http.Request) (userID string, err error) {
+	store, err := session.Start(nil, w, r)
+	if err != nil {
+		return
+	}
+
+	uid, ok := store.Get("loggedInUserID")
+	if !ok {
+		if r.Form == nil {
+			r.ParseForm()
+		}
+
+		store.Set("ReturnUri", r.Form)
+		store.Save()
+
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+
+	userID = uid.(string)
+	store.Delete("loggedInUserID")
+	store.Save()
+	return
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	store, err := session.Start(nil, w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if r.Method == "POST" {
+		store.Set("loggedInUserId", "000000")
+		store.Save()
+
+		w.Header().Set("Location", "/auth")
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	outputHTML(w, r, "static/login.html")
+}
+
+func authHandler(w http.ResponseWriter, r *http.Request) {
+	store, err := session.Start(nil, w, r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if _, ok := store.Get("loggedInUserId"); !ok {
+		w.Header().Set("Location", "/login")
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+
+	outputHTML(w, r, "static/auth.html")
+}
+
+func outputHTML(w http.ResponseWriter, req *http.Request, filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer file.Close()
+	fi, _ := file.Stat()
+	http.ServeContent(w, req, file.Name(), fi.ModTime(), file)
 }
