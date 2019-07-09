6e3fcb791a9ffecc655113c6d7b3644cd6e27d74
Vincent Shen <edentidus@foxmail.com>
2019-07-09 06:33:28 +0000

    consumer of player_reception

---
+++ broker/client.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"github.com/Shopify/sarama"
+	k "github.com/vincent-scw/gframe/broker/kafka"
+)
+
+func main() {
+	log.Println("Starting kafka broker...")
+
+	version, err := sarama.ParseKafkaVersion("2.1.1")
+	if err != nil {
+		log.Panicf("Error parsing Kafka version: %v", err)
+	}
+
+	consumer := k.Consumer{
+		Ready: make(chan bool, 0),
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	config := sarama.NewConfig()
+	config.Version = version
+	config.Consumer.Offsets.Initial = sarama.OffsetOldest
+
+	client, err := sarama.NewConsumerGroup([]string{"40.83.112.48:9092"}, "player_broker", config)
+	if err != nil {
+		log.Panicf("Error creating consumer group client: %v", err)
+	}
+
+	wg := &sync.WaitGroup{}
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		for {
+			if err := client.Consume(ctx, []string{"player"}, &consumer); err != nil {
+				log.Panicf("Error form consumer: %v", err)
+			}
+			if ctx.Err() != nil {
+				return
+			}
+			consumer.Ready = make(chan bool, 0)
+		}
+	}()
+
+	<-consumer.Ready // Await till the consumer has been set up
+	log.Println("Sarama consumer up and running...")
+
+	sigterm := make(chan os.Signal, 1)
+	signal.Notify(sigterm, syscall.SIGINT, syscall.SIGTERM)
+	select {
+	case <-ctx.Done():
+		log.Println("terminating: context cancelled")
+	case <-sigterm:
+		log.Println("terminating: via signal")
+	}
+	cancel()
+	wg.Wait()
+	if err = client.Close(); err != nil {
+		log.Panicf("Error closing client: %v", err)
+	}
+}
---
+++ broker/kafka/consumer.go
@@ -0,0 +1,33 @@
+package kafka
+
+import (
+	"log"
+
+	"github.com/Shopify/sarama"
+)
+
+// Consumer represents a Sarama consumer group
+type Consumer struct {
+	Ready chan bool
+}
+
+// Setup is run at the begining of a new session, before ConsumeClaim
+func (consumer *Consumer) Setup(sarama.ConsumerGroupSession) error {
+	close(consumer.Ready)
+	return nil
+}
+
+// Cleanup is run at the end of a session, once all ConsumeClaim goroutines have exited
+func (consumer *Consumer) Cleanup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+// ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
+func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for message := range claim.Messages() {
+		log.Printf("Message claimed: value = %s, timestamp = %v, topic = %s", string(message.Value), message.Timestamp, message.Topic)
+		session.MarkMessage(message, "")
+	}
+
+	return nil
+}
