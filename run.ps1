b02efcf718bc44e9b968a75a107f279cbd619376
Vincent Shen <edentidus@foxmail.com>
2019-07-26 14:39:16 +0000

    update run script

--- run.ps1
+++ run.ps1
@@ -1,7 +1,19 @@
-invoke-expression 'cmd /c start powershell -Command { go run .\oauth }'
-invoke-expression 'cmd /c start powershell -Command { go run .\broker_svc }'
-invoke-expression 'cmd /c start powershell -Command { go run .\notification_svc }'
-invoke-expression 'cmd /c start powershell -Command { go run .\reception_svc }'
-invoke-expression 'cmd /c start powershell -Command { cd .\playercli; npm start }'
-invoke-expression 'cmd /c start powershell -Command { cd .\admincli; npm start }'
-invoke-expression 'cmd /c start powershell -Command { go run .\admin_notification_svc }'
+param (
+	[bool] $i = 1
+)
+
+workflow _Wkf_Start {
+	parallel {
+		Invoke-Expression -Command "go run .\oauth" 
+		Invoke-Expression -Command "go run .\broker_svc" 
+		Invoke-Expression -Command "go run .\notification_svc"
+		Invoke-Expression -Command "go run .\reception_svc"
+		Invoke-Expression -Command "go run .\admin_notification_svc"
+		if (isInt($i) -eq 1) {
+			Invoke-Expression -Command "cd .\playercli; npm start"
+			Invoke-Expression -Command "cd .\admincli; npm start"
+		}
+	}
+}
+
+_Wkf_Start
