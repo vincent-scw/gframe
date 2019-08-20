f3bb7eb4f5b4fc2c3ae942decff226389e8ce1f6
Vincent Shen <edentidus@foxmail.com>
2019-08-20 01:23:11 +0000

    sould be fine now

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,7 +3,7 @@
 VERSION_NUMBER=$1
 echo "Travis version number is "$VERSION_NUMBER
 
-for f in ./apps/*.yaml
+for f in ./deploy/apps/*.yaml
 do
   template=`cat $f | sed "s/{{VERSION_NUMBER}}/$VERSION_NUMBER/g"`
   echo "$template" | kubectl apply -f -
