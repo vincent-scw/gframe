609901858cec184bed68672ed38c117f237df004
Vincent Shen <edentidus@foxmail.com>
2019-09-23 08:35:57 +0000

    add findgames

--- services/admin_svc/repository/game.go
+++ services/admin_svc/repository/game.go
@@ -7,30 +7,30 @@ import (
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	
+
+	s "github.com/vincent-scw/gframe/admin_svc/singleton"
 	c "github.com/vincent-scw/gframe/contracts"
 	u "github.com/vincent-scw/gframe/util"
-	s "github.com/vincent-scw/gframe/admin_svc/singleton"
 )
 
 // GameModel represents a game in database
 type GameModel struct {
 	// Use generated id instead of bson ObjectID
-	ID			string 	`bson:"_id" json:"id,omitempty"`
-	CreatedBy 	string	`bson:"createdBy" json:"createdBy"`
-	Name 		string	`bson:"name" json:"name"`
-	RegisterTime	time.Time	`bson:"registerTime" json:"registerTime"`
-	StartTime	time.Time	`bson:"startTime" json:"startTime"`
-	Winner 		*c.User	`bson:"winner" json:"winner"`
-	Type		int		`bson:"type" json:"type"`
-	IsCancelled	bool	`bson:"isCancelled" json:"isCancelled"`
+	ID           string    `bson:"_id" json:"id,omitempty"`
+	CreatedBy    string    `bson:"createdBy" json:"createdBy"`
+	Name         string    `bson:"name" json:"name"`
+	RegisterTime time.Time `bson:"registerTime" json:"registerTime"`
+	StartTime    time.Time `bson:"startTime" json:"startTime"`
+	Winner       *c.User   `bson:"winner" json:"winner"`
+	Type         int       `bson:"type" json:"type"`
+	IsCancelled  bool      `bson:"isCancelled" json:"isCancelled"`
 }
 
 // NewGame return a game model
 func NewGame(name, createdBy string, reg time.Time) *GameModel {
 	model := GameModel{
-		ID: 		  u.NextRandom(),
-		Name:	      name,
+		ID:           u.NextRandom(),
+		Name:         name,
 		CreatedBy:    createdBy,
 		RegisterTime: reg,
 		Type:         1,
@@ -42,7 +42,7 @@ func NewGame(name, createdBy string, reg time.Time) *GameModel {
 // GameRepository represents game
 type GameRepository struct {
 	mongoClient *mongo.Client
-	ctx 		context.Context
+	ctx         context.Context
 }
 
 // NewGameRepository returns GameRepository
@@ -50,7 +50,7 @@ func NewGameRepository() *GameRepository {
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	repo := GameRepository{
 		mongoClient: s.GetMongoClient(),
-		ctx: ctx,
+		ctx:         ctx,
 	}
 	return &repo
 }
@@ -76,6 +76,31 @@ func (repo *GameRepository) UpdateGame(model *GameModel) error {
 	return err
 }
 
-func (repo *GameRepository) getCollection() *mongo.Collection{
+// FindGames returns games for created user
+func (repo *GameRepository) FindGames(createdBy string) ([]*GameModel, error) {
+	coll := repo.getCollection()
+	cur, err := coll.Find(repo.ctx, bson.M{"createdBy": createdBy})
+	if err != nil {
+		log.Printf("find games error: %v", err)
+		return nil, err
+	}
+	defer cur.Close(repo.ctx)
+	var games []*GameModel
+	for cur.Next(repo.ctx) {
+		var result GameModel
+		err := cur.Decode(&result)
+		if err != nil {
+			log.Printf("find games error: %v", err)
+			return nil, err
+		}
+		games = append(games, &result)
+	}
+	if err := cur.Err(); err != nil {
+		log.Printf("error %v", err)
+	}
+	return games, err
+}
+
+func (repo *GameRepository) getCollection() *mongo.Collection {
 	return repo.mongoClient.Database("gframe").Collection("games")
 }
--- services/admin_svc/repository/game_test.go
+++ services/admin_svc/repository/game_test.go
@@ -3,8 +3,7 @@ package repository
 import (
 	"testing"
 	"time"
-
-	c "github.com/vincent-scw/gframe/contracts"
+	//c "github.com/vincent-scw/gframe/contracts"
 	//"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -15,12 +14,7 @@ func TestNewGame(t *testing.T) {
 	}
 }
 
-func TestCreateGame(t *testing.T) {
-	model := GameModel{"test", "testg", "Test Game", 
-	time.Date(2009, time.November, 10, 23, 12, 0, 0, time.UTC), 
-		time.Date(2009, time.November, 10, 23, 12, 0, 0, time.UTC),
-		&c.User{Id:"aabbcc", Name:"vincent"},1, false,
-		}
-	repo := NewGameRepository()
-	repo.UpdateGame(&model)
-}
+// func TestCreateGame(t *testing.T) {
+// 	repo := NewGameRepository()
+// 	repo.FindGames("testg")
+// }
